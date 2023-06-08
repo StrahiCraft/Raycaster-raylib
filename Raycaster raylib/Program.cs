ef1697c39a1e91dbd681c3c5ee79a7f88b94e859
StrahiCraft <strahi04@gmail.com>
2023-06-08 11:55:12 +0200

    Add project files.

---
+++ Raycaster raylib/Program.cs
@@ -0,0 +1,23 @@
+﻿using Raylib_cs;
+
+namespace Raycaster_raylib
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Raylib.InitWindow(300, 300, "Raycaster");
+            while(!Raylib.WindowShouldClose())
+            {
+                Raylib.BeginDrawing();
+                Raylib.ClearBackground(Color.BLACK);
+
+                Raylib.DrawLine(0, 0, 300, 300, Color.WHITE);
+
+                Raylib.EndDrawing();
+            }
+
+            Raylib.CloseWindow();
+        }
+    }
+}
